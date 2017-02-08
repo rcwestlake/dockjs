dadfc7e3185862add512169bd06db3b99448e270
Ryan Westlake <ryancwestlake@gmail.com>
2017-02-07 21:14:21 -0700

    [CLEAN]: remove old codemirror version, already able to get value from editor

--- app/renderer.js
+++ app/renderer.js
@@ -4,23 +4,10 @@ const babel = require('babel-core')
 const $input = $('#input')
 const $runButton = $('#run-button')
 
-/* this works, currently using refactored version below
-   use this if we can't pull value from field */
-
-// let editor = CodeMirror(function(elt) {
-//   debugger
-//   $input[0].parentNode.replaceChild(elt, $input[0]);
-// }, {
-//   value: "function myScript(){return 100;}\n",
-//   lineNumbers: true,
-//   mode: "javascript"
-// });
-
 const editor = CodeMirror.fromTextArea($input[0], {
     lineNumbers: true,
     lineWrapping: true,
     theme: 'mbo',
-    value: 'function myScript(){return 100;}\n',
     mode: 'javascript'
 })
 

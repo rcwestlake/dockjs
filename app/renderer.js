fcf971408e7cb6cbc859b2bb7f097218f85c074c
Ryan Westlake <ryancwestlake@gmail.com>
2017-02-07 21:02:17 -0700

    [ADD]: transform es6 code into es5 with babel.transform

--- app/renderer.js
+++ app/renderer.js
@@ -1,11 +1,13 @@
 const $ = require('jquery')
+const babel = require('babel-core')
 
 const $input = $('#input')
+const $runButton = $('#run-button')
 
 /* this works, currently using refactored version below
    use this if we can't pull value from field */
 
-// let myCodeMirror = CodeMirror(function(elt) {
+// let editor = CodeMirror(function(elt) {
 //   debugger
 //   $input[0].parentNode.replaceChild(elt, $input[0]);
 // }, {
@@ -14,10 +16,18 @@ const $input = $('#input')
 //   mode: "javascript"
 // });
 
-var myCodeMirror = CodeMirror.fromTextArea($input[0], {
+const editor = CodeMirror.fromTextArea($input[0], {
     lineNumbers: true,
     lineWrapping: true,
     theme: 'mbo',
-    value: "function myScript(){return 100;}\n",
-    mode: "javascript"
-});
+    value: 'function myScript(){return 100;}\n',
+    mode: 'javascript'
+})
+
+$runButton.on('click', () => {
+  const code = editor.getValue()
+  const result = babel.transform(code, {
+    presets: ['es2015']
+  })
+  console.log(result);
+})

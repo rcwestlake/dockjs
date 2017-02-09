c53a8669646cc9db5f7f256a6d8559ce569c1a6d
Ryan Westlake <ryancwestlake@gmail.com>
2017-02-09 11:22:55 -0700

    [ADD]: switch to menubar app

--- app/main.js
+++ app/main.js
@@ -1,24 +1,39 @@
 const { app, BrowserWindow } = require('electron')
+const menubar = require('menubar')
 const mock = require('../tests/mocks')
 
-let mainWindow = null
-
-app.on('ready', () => {
-  console.log('app is ready')
-  mainWindow = new BrowserWindow({
-    maxWidth: 800,
-    maxHeight: 600,
-    // webPreferences: { devTools: true }
-  })
-
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show()
-  })
-
-  mainWindow.webContents.openDevTools()
-
-  mainWindow.loadURL(`file://${__dirname}/index.html`)
+const mb = menubar({
+  width: 800,
+  height: 600
 })
+
+mb.on('ready', function ready () {
+  console.log('dockjs is ready')
+})
+
+mb.on('after-create-window', () => {
+  mb.window.loadURL(`file://${__dirname}/index.html`)
+  mb.window.webContents.openDevTools()
+})
+
+// let mainWindow = null
+//
+// app.on('ready', () => {
+//   console.log('app is ready')
+//   mainWindow = new BrowserWindow({
+//     maxWidth: 800,
+//     maxHeight: 600,
+//     // webPreferences: { devTools: true }
+//   })
+//
+//   mainWindow.once('ready-to-show', () => {
+//     mainWindow.show()
+//   })
+//
+  // mainWindow.webContents.openDevTools()
+//
+//   mainWindow.loadURL(`file://${__dirname}/index.html`)
+// })
 
 // BrowserWindow.addDevToolsExtension(path)
 
